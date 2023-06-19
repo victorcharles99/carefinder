a69a5939c379b08ec98f296b8de2faffabd7e2b6
Akande Gbolahan <akandegbolahanayomide@gmail.com>
2023-06-19 12:12:55 -0700

    Added Pictures

--- src/components/Landing Page/SectionOne.tsx
+++ src/components/Landing Page/SectionOne.tsx
@@ -5,7 +5,7 @@ import {AiOutlineSearch} from 'react-icons/ai'
 
 const SectionOne = () => {
   return (
-    <section className="bg-[#E0E4EC]">
+    <section className="bg-[#E0E4EC] pb-5">
         {/* Container */}
         <div className="pl-5 py-10 grid grid-cols-[1fr_1.5fr] gap-5">
             {/* Grid Text */}
@@ -24,7 +24,7 @@ const SectionOne = () => {
         </div>
 
         <div>
-            <p className='text-center text-[#08299B] font-bold'>Find a nearby hospital</p>
+            <p className='text-center text-[#08299B] font-bold text-2xl'>Find a nearby hospital</p>
             <div className="w-1/2 mx-auto mt-1 mb-5 block ">
                 <CiLocationOn color="#C1C1C1" class="translate-y-7 translate-x-5"/>
                 <input type="text" placeholder="Federal Medical Center, Ebutte Metta,Lagos State" className=" rounded-2xl p-2 text-center text-[#C1C1C1] font-thin text-md bg-white w-full" required/>
--- src/components/Landing Page/SectionTwo.tsx
+++ src/components/Landing Page/SectionTwo.tsx
@@ -1,30 +1,56 @@
 import IMG1 from  '../../assets/Landing Page/Section2.1.png'
 import IMG2 from  '../../assets/Landing Page/Section2.2.png'
+import Hospital from '../../assets/Landing Page/hospital.jpg'
+import Doctor from '../../assets/Landing Page/chatDoctor.png'
+import Export from '../../assets/Landing Page/export.png'
+import Share from '../../assets/Landing Page/share.png'
 
 const SectionTwo = () => {
   return (
-    <section className="bg-white -mt-5">
-        <div className=" py-10 grid grid-cols-[1.8fr_1fr]">
-            {/* Grid Image */}
-            <article className='border border-purple-600'>
-                <div className='flex'>
-                <img src={IMG1} alt="A patient pressing her phone" className='w-[24.25rem] h-[44rem]'/>
-                <img src={IMG2} alt="A woman checking her bp by a doctor" className='w-[24.25rem] h-[44rem] ml-4 mt-9'/>
-                </div>
-            </article>
-            {/* Grid Text */}
-            <article className='border border-red-500'>
-                <div className='border border-green-500 mt-9 flex flex-col justify-between h-[40rem] text-center'>
-                    <div className='text-center text-black font-bold text-4xl'>
-                        <p>Welcome to</p> <br /> 
-                        <h2 className='text-[#08299B]  bg-red-500'>Carefinder</h2>
+    <>
+        <section className="bg-white -mt-5">
+            <div className=" py-10 grid grid-cols-[1.8fr_1fr]">
+                {/* Grid Image */}
+                <article className=''>
+                    <div className='flex'>
+                    <img src={IMG1} alt="A patient pressing her phone" className='w-[24.25rem] h-[44rem]'/>
+                    <img src={IMG2} alt="A woman checking her bp by a doctor" className='w-[24.25rem] h-[44rem] ml-4 mt-9'/>
                     </div>
-                    <p className='text-center text-black text-3xl font-medium'>Carefinder is a platform where users can search for hosiptals in their areas, export hospital details for your records and enhance your healthcare experience by connecting with others and sharing valuable resources.</p>
-                    <button className="uppercase bg-[#08299B] text-white text-2xl p-2 italic w-1/2 mx-auto">Our Services</button>
-                </div>
-            </article>
-        </div>
-    </section>
+                </article>
+                {/* Grid Text */}
+                <article className=''>
+                    <div className=' mt-9 flex flex-col justify-between h-[40rem] text-center'>
+                        <div className='text-center text-black font-bold text-4xl'>
+                            <p>Welcome to</p> <br /> 
+                            <h2 className='text-[#08299B]  bg-red-500'>Carefinder</h2>
+                        </div>
+                        <p className='text-center text-black text-3xl font-medium'>Carefinder is a platform where users can search for hosiptals in their areas, export hospital details for your records and enhance your healthcare experience by connecting with others and sharing valuable resources.</p>
+                        <button className="uppercase bg-[#08299B] text-white text-2xl p-2 italic w-1/2 mx-auto">Our Services</button>
+                    </div>
+                </article>
+            </div>
+
+            {/* Article Two */}
+            <section className='grid grid-cols-4 my-6 gap-3'>
+                {/* Card One */}
+                <article className='bg-[#E0E4EC] text-center pt-2 pb-5 rounded-lg'>
+                    <div className='w-1/2 mx-auto bg-red-500 border border-green-600'>
+                        <div className='bg-[#08299B] w-[5rem] aspect-square rounded-full mx-auto'>
+                            <img src={Hospital} alt="Hospital" className='w-2/3 aspect-square mx-auto pt-2'/>
+                        </div>
+                        <p className='font-bold text-base py-3 text-center'>Search Doctors</p>
+                        <p className='text-xs font-medium text-center py-3'>Effortlessly find the best hospital near you</p>
+                    </div>
+                </article>
+                {/* Card Two */}
+                <article></article>
+                {/* Card Three */}
+                <article></article>
+                {/* Card Four */}
+                <article></article>
+            </section>
+        </section>
+    </>
   )
 }
 
