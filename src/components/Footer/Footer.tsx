b8d0a40c1a78d5ae7c75d86a09066247694e1dab
Akande Gbolahan <akandegbolahanayomide@gmail.com>
2023-06-20 22:25:04 -0700

    commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <section className="px-10 bg-[#E0E4EC]">
+        <article className="grid grid-cols-3">
+            <div>
+                <h3>CareFinder</h3>
+                <p>Plot 42, Akinza Street, Victoria Island, Lagos.
+                    +2349167351788
+                </p>
+            </div>
+            <div>
+                <h3>About Us</h3>
+                <p><a href="#">News & Media</a></p>
+                <p><a href="#">Contact Us</a></p>
+            </div>
+            <div>
+                <h3>Quick Links</h3>
+                <p><a href="#">My account</a></p>
+                <p><a href="#">Book an appointment</a></p>
+                <p><a href="#">Library</a></p>
+            </div>
+        </article>
+    </section>
+  )
+}
+
+export default Footer
--- src/components/Landing Page/SectionTwo.tsx
+++ src/components/Landing Page/SectionTwo.tsx
@@ -22,7 +22,7 @@ const SectionTwo = () => {
                     <div className=' mt-9 flex flex-col justify-between h-[40rem] text-center'>
                         <div className='text-center text-black font-bold text-4xl'>
                             <p>Welcome to</p> <br /> 
-                            <h2 className='text-[#08299B]  bg-red-500'>Carefinder</h2>
+                            <h2 className='text-[#08299B]'>Carefinder</h2>
                         </div>
                         <p className='text-center text-black text-3xl font-medium'>Carefinder is a platform where users can search for hosiptals in their areas, export hospital details for your records and enhance your healthcare experience by connecting with others and sharing valuable resources.</p>
                         <button className="uppercase bg-[#08299B] text-white text-2xl p-2 italic w-1/2 mx-auto">Our Services</button>
@@ -33,21 +33,45 @@ const SectionTwo = () => {
             {/* Article Two */}
             <section className='grid grid-cols-4 my-6 gap-3'>
                 {/* Card One */}
-                <article className='bg-[#E0E4EC] text-center pt-2 pb-5 rounded-lg'>
-                    <div className='w-1/2 mx-auto bg-red-500 border border-green-600'>
+                <article className='bg-[#E0E4EC] text-center ml-2 pt-2 pb-5 rounded-lg text-black'>
+                    <div className='w-1/2 mx-auto'>
                         <div className='bg-[#08299B] w-[5rem] aspect-square rounded-full mx-auto'>
                             <img src={Hospital} alt="Hospital" className='w-3/4 aspect-square mx-auto pt-2'/>
                         </div>
-                        <p className='font-bold text-base py-3 text-center'>Search Doctors</p>
-                        <p className='text-xs font-medium text-center py-3'>Effortlessly find the best hospital near you</p>
+                        <p className='font-bold text-base py-3 text-center'>Search Hospitals</p>
+                        <p className='text-xs font-medium text-center py-3'>Effortlessly Find the Best hospital Near You</p>
                     </div>
                 </article>
                 {/* Card Two */}
-                <article></article>
+                <article className='bg-[#E0E4EC] text-center  pt-2 pb-5 rounded-lg text-black'>
+                    <div className='w-1/2 mx-auto'>
+                        <div className='bg-[#08299B] w-[5rem] aspect-square rounded-full mx-auto'>
+                            <img src={Doctor} alt="Doctor" className='w-3/4 aspect-square mx-auto pt-2'/>
+                        </div>
+                        <p className='font-bold text-base py-3 text-center'>Search Doctors</p>
+                        <p className='text-xs font-medium text-center py-3'>EffortlesslyFfind the Best Doctors Near You</p>
+                    </div>
+                </article>
                 {/* Card Three */}
-                <article></article>
+                <article className='bg-[#E0E4EC] text-center  pt-2 pb-5 rounded-lg text-black'>
+                    <div className='w-1/2 mx-auto'>
+                        <div className='bg-[#08299B] w-[5rem] aspect-square rounded-full mx-auto'>
+                            <img src={Export} alt="Export" className='w-3/4 aspect-square mx-auto pt-2'/>
+                        </div>
+                        <p className='font-bold text-base py-3 text-center'>Export Hospitals </p>
+                        <p className='text-xs font-medium text-center py-3'>Save list of hospitals.</p>
+                    </div>
+                </article>
                 {/* Card Four */}
-                <article></article>
+                <article className='bg-[#E0E4EC] text-center  pt-2 pb-5 rounded-lg text-black'>
+                    <div className='w-1/2 mx-auto'>
+                        <div className='bg-[#08299B] w-[5rem] aspect-square rounded-full mx-auto'>
+                            <img src={Share} alt="Share" className='w-3/4 aspect-square mx-auto pt-2'/>
+                        </div>
+                        <p className='font-bold text-base py-3 text-center'>Share Hospitals</p>
+                        <p className='text-xs font-medium text-center py-3'>Share the list of hospitals with others.</p>
+                    </div>
+                </article>
             </section>
         </section>
     </>
