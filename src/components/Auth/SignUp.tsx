fd8bf295d5a9c5605aeb2faa99be8d9a6b2a4b52
Akande Gbolahan <akandegbolahanayomide@gmail.com>
2023-06-26 17:30:48 +0100

    styling

--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -1,11 +1,13 @@
 // import Stethoscope from '../../assets/Auth/Stethoscope.png'
 import Injection from '../../assets/Auth/Injection.png'
 import {FcGoogle} from 'react-icons/fc'
+import {BsApple} from 'react-icons/bs'
+import {FaFacebook} from 'react-icons/fa'
 
 const SignUp = () => {
   return (
     <>
-    <section className="bg-[#E0E4EC] text-black text-center text-xl h-screen w-full grid grid-cols-2">
+    <section className="bg-[#E0E4EC] text-black text-center text-xl h-auto w-full grid grid-cols-2">
       <article className="bg-white rounded-2xl ml-9 my-[4rem]">
         <h2 className='text-center font-bold text-4xl my-7'>Create An Account</h2>
         <form className='flex flex-col gap-3 pl-7'>
@@ -18,12 +20,22 @@ const SignUp = () => {
           <button className='bg-[#08299B] text-white text-xl w-1/2 mx-auto p-2 rounded-xl'>Create Account</button>
           <p className='w-1/2 mx-auto font-semibold text-2xl text-black'>OR</p>
             {/* ICONS  */}
-          <div>
-            <FcGoogle/>
+          <div className='w-1/2 mx-auto flex gap-6 justify-center'>
+            <FcGoogle size="35"/>
+            <BsApple color="#000" size="35"/>
+            <FaFacebook color="#1877F2" size="35"/>
           </div>
+          <p className='my-3'>Already have an account? <a className='text-[#08299B]' href="#">Login</a></p>
         </form>
       </article>
-      <article className='bg-Sthethoscope'></article>
+      <article className='bg-Sthethoscope'>
+        <h1 className='text-[#08299B] text-4xl font-bold text-center py-8'>Carefinder</h1>
+        <h3 className='text-4xl font-bold text-center py-4 w-1/2 mx-auto leading-snug'>Join Our Community</h3>
+        <p className='text-[#333232] text-xl font-semibold text-center py-2 w-1/2 mx-auto'>Enjoy seamless access to medical services</p>
+        <div className='pb-12 pt-2'>
+          <img src={Injection} alt="A nurse giving Injection to her patient" />
+        </div>
+      </article>
     </section>
     </>
   )
--- src/components/Landing Page/SectionOne.tsx
+++ src/components/Landing Page/SectionOne.tsx
@@ -1,4 +1,5 @@
 import IMG1 from '../../assets/Landing Page/Section1.jpg'
+import Rec1 from '../../assets/Landing Page/Rectangle 1.png'
 import {AiOutlineArrowRight} from 'react-icons/ai'
 import {CiLocationOn} from 'react-icons/ci'
 import {AiOutlineSearch} from 'react-icons/ai'
@@ -19,7 +20,7 @@ const SectionOne = () => {
              </article>
              {/* Gird Image */}
             <article>
-                <img src={IMG1} alt="A woman receiving treatment" className='h-[38rem] w-[51rem] bg-transparent'/>
+                <img src={Rec1} alt="A woman receiving treatment" className='h-[38rem] w-[51rem] bg-transparent'/>
             </article>
         </div>
 
@@ -28,7 +29,7 @@ const SectionOne = () => {
             <div className="w-1/2 mx-auto mt-1 mb-5 block ">
                 <CiLocationOn color="#C1C1C1" class="translate-y-7 translate-x-5"/>
                 <input type="text" placeholder="Federal Medical Center, Ebutte Metta,Lagos State" className=" rounded-2xl p-2 text-center text-[#C1C1C1] font-thin text-md bg-white w-full" required/>
-                <AiOutlineSearch color="#000" class="-translate-y-7 translate-x-[39rem]"/>
+                <AiOutlineSearch color="#000" class="-translate-y-8 translate-x-[39rem]"/>
             </div>
         </div>
     </section>
