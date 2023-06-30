b331150506491b222780c8c3db8b5c0bd450d072
Akande Gbolahan <akandegbolahanayomide@gmail.com>
2023-06-30 18:23:13 +0100

    How it works

--- src/components/Landing Page/SectionThree.tsx
+++ src/components/Landing Page/SectionThree.tsx
@@ -2,15 +2,21 @@ import Hospital from '../../assets/Landing Page/hospital.png'
 import Doctor from '../../assets/Landing Page/chatDoctor.png'
 import Export from '../../assets/Landing Page/export.png'
 import Share from '../../assets/Landing Page/share.png'
-// import Biggest from '../../assets/'
 import Biggest from '../../assets/Landing Page/How it works/BiggestEclipse.png'
 import Bigger from '../../assets/Landing Page/How it works/BiggerEclipse.png'
 import Big from '../../assets/Landing Page/How it works/BigEclipse.png'
+import FindHospital from '../../assets/Landing Page/How it works/FindHospital.png'
+import SaveHospital from '../../assets/Landing Page/How it works/SaveHospital.png'
+import BookAppointment from '../../assets/Landing Page/How it works/BookAppointment.png'
+import MakeVisit from '../../assets/Landing Page/How it works/MakeVisit.png'
+import Arrow1 from '../../assets/Landing Page/How it works/Arrow 1.png'
+import Arrow2 from '../../assets/Landing Page/How it works/Arrow 2.png'
+import Arrow3 from '../../assets/Landing Page/How it works/Arrow 3.png'
 
 
 const SectionThree = () => {
   return (
-    <section className="bg-white border border-red-500">
+    <section className="bg-white border border-red-500 text-black">
         {/* CARDS */}
         <section className='grid grid-cols-4 my-6 pb-8 gap-3'>
                 {/* Card One */}
@@ -57,14 +63,44 @@ const SectionThree = () => {
 
             {/* How It Works */}
 
-            <section className=' bg-red-500 border border-green-500 h-screen'>
-                {/* <article className='w-11/12 aspect-square'> */}
-                    <div className='bg-BiggestEclipse w-auto h-4/5 relative'>
-                        <div className='bg-BiggerEclipse w-2/3 h-full'></div>
-                        {/* <div className='bg-BiggerEclipse absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-full'>Hey</div> */}
-                        <div className='bg-BigEclipse'></div>
+            <section className='  border border-green-500  overflow-hidden pb-[5rem] relative'>
+                <div className='absolute index-0 z-20 flex items-center justify-between w-4/5 h-full  mx-auto left-0 right-0'>
+                    <div className=' ml-[3rem] -mt-[5rem] w-1/4'>
+                        <img src={FindHospital} alt="Find Hospital" />
+                        <h3 className='font-bold text-base py-2'>Find Hospital</h3>
+                        <p className='font-medium w-2/3 py-2 text-xs'>Find the best hospitals and doctors near you, our efficient search engine provides you with the best results.</p>
                     </div>
-                {/* </article> */}
+                    <img src={Arrow1} alt="Arrow 1" className='w-[8rem] ml-[3rem] -mt-[9rem] transform rotate-6 scale-150 ' />
+                    <div className=' mt-[16rem] ml-[1rem] w-1/2'>
+                        <img src={SaveHospital} alt="Save Hospital" />
+                        <h3 className='font-bold text-base py-2 w-1/2 text-center'>Save or share hospital</h3>
+                        <p className='font-medium w-1/2 py-2 text-xs text-center'>Carefinder allows users to save and share the list of hospitals with others.
+                        Users can share the information via email or by generating a shareable link</p>
+                    </div>
+                    <img src={Arrow2} alt="" className='w-[8rem] -ml-[15rem] -mt-[5rem]  scale-150 transform -rotate-[17deg]' />
+                    <div className=' -mt-[10rem] -mr-[9rem]'>
+                        <img src={BookAppointment} alt="Book Appointment" />
+                        <h3 className='font-bold text-base py-2 w-2/3'>Book Appointment</h3>
+                        <p className='font-medium w-1/2 py-1 text-xs text-center'>Book appointments easily by inputting correct details</p>
+                    </div>
+                    <img src={Arrow3} alt="" className='w-[10rem] ml-[7rem] -mt-[2rem] transform -rotate-[4deg] scale-150'/>
+                    <div className=' mt-[20rem] -mr-[10rem] flex flex-col items-center '>
+                        <img src={MakeVisit} alt="Make a visit" />
+                        <h3 className='font-bold text-base text-center py-2 w-2/3'>Make a Visit</h3>
+                        <p className='font-medium w-1/2 py-1 text-xs text-center'>Appointment confirmed, then make a visit to your selected hospital or doctor</p>
+                    </div>
+                </div>
+                <img src={Biggest} alt="Biggest Eclipse" className='w-full h-auto'/>
+                <h1 className='absolute inset-0 flex justify-center font-bold text-4xl text-[#08299B] py-[2rem] text-center'>How it works</h1>
+                <div className='absolute inset-0 flex items-center justify-center'>
+                    <img src={Bigger} alt="Bigger Eclipse" className='mt-5'/>
+                    <div className='absolute inset-0 flex items-center justify-center'>
+                        <img src={Big} alt="Big Eclipse" className='mt-[8.5rem]'/>
+                    </div>
+                </div>
+
+                
+                
             </section>
     </section>
   )
